007de6af65bdeee9ec37b3dae1714646ee239071
Gaurav Dwivedi <gauravdwivedi@Gauravs-MacBook-Air.local>
2025-11-12 21:04:36 +0530

    Class four completed

---
+++ Learning_py/class4.py
@@ -0,0 +1,17 @@
+''' 
+Operators 
+Arithmatic  +, -, *, /, //, ** %  (works only in numerical values)
+Assisgnment =, +=, -=, *=, /=, //=, **= 
+String concatination
+'''
+
+#// (float division) In Python, floor division is an operation that divides two numbers and rounds the result down to the nearest integer,
+print("Simple division ", 15/8)
+print("Floor division ", 15//8)
+
+#concatination
+print("Hello" + "JS")
+# print("Hello" + 25)  (we can't do this)
+
+#multiplication
+print("Gaurav\t" * 10) # it will print our string 10 time
