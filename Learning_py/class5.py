68e8b63d32de5fdd98ff19f004c3aa3739052284
Gaurav Dwivedi <gauravdwivedi@Gauravs-MacBook-Air.local>
2025-11-12 21:26:25 +0530

    class five completed

---
+++ Learning_py/class5.py
@@ -0,0 +1,14 @@
+'''
+comparision (Gives result in boolean - true/false)
+==, >, <, >=, <=, !=
+Logical operator 
+and, or, not
+
+'''
+
+print((12 == 12) == True)
+print( 12 == 12 and 13 == 13)
+print( 12 == 12 or 11 == 13)
+print( not 12 == 12 )
+print(not 15 == 13 == False). 
+print(not (15 == 13 == False))
