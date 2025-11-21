ab954a0472eb907337f5937e80fba071a5fd9f7f
Gaurav Dwivedi <gauravdwivedi@Gauravs-MacBook-Air.local>
2025-11-21 21:20:33 +0530

    class 10 completed

---
+++ Learning_py/class10.py
@@ -0,0 +1,13 @@
+'''
+Data Structures
+List, Tuple, Set, Dictionary
+LinkedList, Stack, Queue, Tree, Graph etc.
+'''
+
+list = [1,2,3,4,5]
+tuple = (1,2,3,4,5)
+
+dictionary = {
+    "name": "Gaurav",
+    "age": 23
+}
