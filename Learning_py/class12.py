8ffe8848291cffc5b752b60b673946bec87aab67
Gaurav Dwivedi <gauravdwivedi@Gauravs-MacBook-Air.local>
2025-11-23 23:33:03 +0530

    class 12 tuple

---
+++ Learning_py/class12.py
@@ -0,0 +1,12 @@
+#tuple - immutable, same as list, implemented by ()
+#tuple unpacking
+
+tp = (1,20,40,11,56,78)
+# tp[1] = 25 --> not allowed
+
+#tuple unpacking
+
+a,b,c = (10,30,50)
+
+a = (30) #unpacking happenig
+a = (30,) # now unpacking not happening
