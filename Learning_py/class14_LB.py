e1e020af52c929df4182cc7fb8ea0b9cd9f3609c
Gaurav Dwivedi <gauravdwivedi@Gauravs-MacBook-Air.local>
2025-11-29 21:39:38 +0530

    leetcode 1882

--- Learning_py/class14_LB.py
+++ Learning_py/class14_LB.py
@@ -1,4 +1,6 @@
-# count the frequency of each number given in list
+# 1.count the frequency of each number given in list
+#leet code 771 (did in leetoce)
+#leetcode 1832 (did in leetcode)
 
 #Solution 1 
 list = [1,1,1,2,2,2,2,3,5,5,5,6,6,6,1,1,2,3,5,6,6,7,8,9,1,2,3,4,5]
@@ -24,3 +26,17 @@ for item in list:
         
 print("Solution -2 ", dic)
 
+#leetcode 1832
+
+'''class Solution(object):
+    def checkIfPangram(self, sentence):
+        dec = {}
+        for item in sentence:
+            if(item in dec.keys()):
+                dec[item] +=1
+            else:
+                dec[item] = 1
+        if(len(dec) == 26):
+            return True
+        else:
+            return False'''
