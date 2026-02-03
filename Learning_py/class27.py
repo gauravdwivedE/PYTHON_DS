676c8f13dbcb351a71c7f405d8cc258dbd2d221a
Gaurav Dwivedi <gauravdwivedi.mail@gmail.com>
2026-02-03 08:56:53 +0530

    class 27 has been completed

--- Learning_py/class27.py
+++ Learning_py/class27.py
@@ -1,3 +1,49 @@
 '''
-Dunder/ magical method
+Dunder/ magical metho
+
+# __str__
+
+#__len__
+
+#__add__
+
 '''
+
+#__str__ example
+
+class Book:
+    def __init__(self, title):
+        self.title = title
+
+    def __str__(self):
+        return self.title
+
+
+b1 = Book("Python Programming")
+print(b1)
+
+#__len__ example   
+
+class Shopping:
+    def __init__(self, items):
+        self.items = items
+
+    def __len__(self):
+        return len(self.items)
+
+shop = Shopping(['apple', 'banana', 'orange'])
+print(len(shop))
+
+
+#__add__ example
+
+class Number:
+    def __init__(self, value):
+        self.value = value
+
+    def __add__(self, other):
+        return self.value + other.value
+    
+n1 = Number(10)
+n2 = Number(20)
+print(n1 + n2)
